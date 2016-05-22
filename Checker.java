bd25e888dac20cd800c3dc3a1f5415c4ff0db087
AysenurKan <aysenurkan95@gmail.com>
2016-05-22 14:22:03 +0300

    Update Checker.java

--- Checker.java
+++ Checker.java
@@ -1,67 +1,58 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
-
 
 public class Checker {
 
 	/* Main class, it will read the wordlist.txt file and store words into a hash table. 
 	*then spell-check an input sentence.*/
 	
-	
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a sentence >>");
-		String input=sc.nextLine();
+		String input = sc.nextLine();
 		
-		File wordList = new File("/home/gulnur/workspace/HW5/src/wordlist.txt");
+//		Define wordList file
+		File wordList = new File("/Users/aysenur/Documents/workspace/CENG_201_HW5/src/WorkChecker/wordlist.txt");
 		
 //		Add Scanner to read from file
 		Scanner list = new Scanner(wordList);
 		
-//		Create a hasher for using hash table 
+//		Create hasher to add words into hash table
 		SampleStringHasher hasher = new SampleStringHasher();
 		
-// 		to find table size
-		int size=0;
+//		Create a hash table
+		HashTable table = new HashTable(2500, hasher);
+		
+		
+		
 		while(list.hasNextLine()){
-			String str = list.nextLine();
-			size++;
+			
+			String s = list.nextLine();
+			table.add(s);
 		}
 		
-//		create a hash table		
-		HashTable table = new HashTable(size, hasher);
-		
-//		store words into a hash table
-		while(list.hasNextLine()){
-			String str = list.nextLine();
-			table.add(str);
-		}
-		
-//		create a word checker inside wordlist.txt
-		WordChecker checker =new WordChecker(table);
+//		Create word checker to check if hash table contains input
+		WordChecker checker = new WordChecker(table);
 		
 		boolean misspelling = false;
 		String[] words = input.split(" ");
+			
+			
 		
 		System.out.println("Result\n ********");
 		
 		for (int i = 0; i < words.length; i ++){
-			if (!(checker.wordExists(words[i]))){
+			if (!checker.wordExists(words[i])){
 				System.out.println("Word not found:" + words[i]);
-				System.out.println("perhaps you meant:" + checker.getSuggestions(words[i]));
+				System.out.println("perhaps you meant:" + checker.printSuggestions(words[i]));
 				misspelling = true;
 			}
-			
 		}
-		if (misspelling = false)
+		if (misspelling == false)
 			System.out.println("No misspelling in the sentence.");
-		
-		
-
 	}
 
 }
